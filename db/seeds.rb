ddfb5c82bc2cd5757f0d5baac99bd7ee33e08e9f
khalid-zein <zeinkhalid004@gmail.com>
2023-03-01 16:24:48 +0300

    setting up seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+puts "Deleting old data..."
+Product.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+user1 = User.create(name: Faker::Name.name)
+user2 = User.create(name: Faker::Name.name)
+user3 = User.create(name: Faker::Name.name)
+
+puts "Creating products..."
+product1 = Product.create(name: "Stapler", price: 10)
+product2 = Product.create(name: "Whiteboard", price: 15)
+product3 = Product.create(name: "Dry Erase Markers", price: 5)
+product4 = Product.create(name: "Ballpoint Pens", price: 2)
+product5 = Product.create(name: "Scotch Tape", price: 3)
+
+puts "Creating reviews..."
+15.times do 
+    user_ids = User.all.map {|user| user.id}
+    product_ids = Product.all.map {|product| product.id}
+
+    Review.create(
+        user_id: user_ids[rand(0..2)],
+        product_id: product_ids[rand(0..4)],
+        star_rating: rand(1..10),
+        comment: Faker::Lorem.sentence(word_count: rand(1...10))
+    )
+end
+
+
+puts "Seeding done!"
